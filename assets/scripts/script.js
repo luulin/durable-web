61ef0a97997a9af20f3e900c3eee2c0829ad84d1
nguyenova <luu.nguyenova@office365.spspzlin.cz>
2024-06-01 00:35:50 +0200

    durable-web

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,3 @@
-
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -15,14 +13,26 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("Gallery");
     var dots = document.getElementsByClassName("dot");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", ""); // Remove "active" class from all dots
     }
+
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+document.querySelector('.prev').addEventListener('click', function() {
+    plusSlides(-1);
+});
+document.querySelector('.next').addEventListener('click', function() {
+    plusSlides(1);
+});
+
+showSlides(slideIndex);
